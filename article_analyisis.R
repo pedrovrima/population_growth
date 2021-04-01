18216c913df7314d39a938054a3f98f12eb9925a
Pedro Martins <pedrovrima@gmail.com>
2021-04-01 10:48:20 -0300

    changes parametrization for f

--- article_analyisis.R
+++ article_analyisis.R
@@ -5,27 +5,46 @@ inits <- function() {
     list(
         alpha.phi = runif(1, -2, 2),
         beta.time.phi = runif(1, -2, 2),
-        alpha.rho = runif(1, -0.5, 0.5),
-        sigma.rho = runif(1, 0, 1),
+        alpha.f = runif(1, -0.5, 0.5),
+        sigma.f = runif(1, 0, 1),
         alpha.p = runif(1, -0.5, 0.5),
         mu = runif(bugs.data.storm$s, 0.3, 1)
     )
 }
 # Define  parameters  to be  monitored
-parameters <- c(
+parameters_f <- c(
     "phi",
     "alpha.phi",
     "mean.phi",
     "beta.time.phi",
     "gamma",
-    "rho",
+    "f",
+    "alpha.f",
+    "mean.f",
+    "sigma.f",
+    "p",
+    "alpha.p",
+    "mean.p",
+    "rho"
+)
+
+parameters_rho <- c(
+    "phi",
+    "alpha.phi",
+    "mean.phi",
+    "beta.time.phi",
+    "gamma",
+    "f",
     "alpha.rho",
     "mean.rho",
     "sigma.rho",
     "p",
     "alpha.p",
-    "mean.p"
+    "mean.p",
+    "rho"
 )
+
+
 # MCMC  settings
 niter <- 100000
 nthin <- 10
